03ea75435bbc9cfb569f2751cffa35b1d05b0ea2
Joel Haker <joeladam@gmail.com>
2018-07-23 21:27:26 -0400

    Found current apt-automate script

--- apt-automate.sh
+++ apt-automate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get -u upgrade
+apt-get update && apt-get -y upgrade
--- update_all.sh
+++ update_all.sh
@@ -1,11 +1,95 @@
 #!/usr/bin/env bash
 
-hosts="machine1 machine2 machine3"
+## What Script was before...
 
-for i in $hosts;
-do
-  echo $i;
-  ssh $i sudo apt-automate;
-  done;
+# hosts="machine1 machine2 machine3"
+# 
+# for i in $hosts; do
+#     echo $i;
+#     ssh $i sudo apt-automate;
+# done;
+# 
+# sudo apt-automate\
 
+## Current version...
+
+## Colors
+RED='\E[0;31m'
+GREEN='\E[0;32m'
+ORANGE='\E[0;33m'
+BLUE='\E[0;34m'
+PURPLE='\E[0;35m'
+CYAN='\E[0;36m'
+LGRAY='\E[0;37m'
+DGRAY='\E[1;30m'
+LRED='\E[1;31m'
+LGREEN='\E[1;32m'
+YELLOW='\E[1;33m'
+LBLUE='\E[1;34m'
+LPURPLE='\E[1;35m'
+LCYAN='\E[1;36m'
+NC='\E[0m'
+
+### True Varabiles
+var_true=true
+var_false=false
+
+#  Function to ssh into a each server and run the script
+#  "apt-automate" living on each server.
+#  
+#  On each server, the user that runs "apt-automate"
+#  needs to be able to run "apt-automate" without having to
+#  enter the sudo password.
+#
+#  ##  The line to enter in visudo on each server    
+#  ##  *user* ALL=(root) NOPASSWD: /usr/local/bin/apt-automate
+#  
+#  The second variable is to indicate wether sudo needs
+#  placed in front of the apt-automate script.
+updateservers()
+{
+    declare -a server=("${!1}")
+    
+    for i in "${server[@]}"; do
+        echo ""
+        echo -e "${CYAN} $i ${NC}"
+        echo ""
+        if [ "$2" = true ]; then
+            ssh $i apt-automate
+        else
+            ssh $i sudo apt-automate
+        fi
+    done;
+}
+
+## Function to list each server and then run updateservers()
+servers()
+{
+    # Root users (don't need sudo)
+    # root users were for linode, but
+    # serverpilot updates server automaticallly.
+    
+    # local rootusrs_list=(
+    #   "root@192.155.92.82"
+    # )    
+    
+    # users that need sudo
+    local sudousers_list=(
+        "joel@192.168.1.5" 
+        "pi@192.168.1.8" 
+        "pi@192.168.1.9" 
+        #"pi@192.168.1.41"
+    )
+    
+    ## Keeping just in case..
+    # updateservers rootusrs_list[@] $var_true
+    updateservers sudousers_list[@] $var_false
+}
+
+## Run servers()
+servers
+    
+echo
+echo -e "${LCYAN} Upadate / Upgrade this computer... ${NC}";
+echo
 sudo apt-automate
